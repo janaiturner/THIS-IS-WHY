53a5ca04ed27623babb584977adc5e46cbedcc24
janaiturner <143103314+janaiturner@users.noreply.github.com>
2023-10-12 04:53:31 -0500

    troubleshooting html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 // JS scripts placed here
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "monday-soap") {
+        x.className += " none";
+    } else {
+        x.className = "monday-soap";
+    }
+}
